a859fc4a5aa8635f6e171a286c76558c8452ab10
Viktor <pozdnyakov151994@gmail.com>
2022-09-19 20:58:22 +0300

    second

--- script.js
+++ script.js
@@ -12,13 +12,13 @@ showFirstMessage("Hello world!");
 console.log(num);
 
 
-function calc(a,b) {
-    return(a+b);
-}
+// function calc(a,b) {
+//     return(a+b);
+// }
 
-console.log(calc(4,3));
-console.log(calc(5,6));
-console.log(calc(10,6));
+// console.log(calc(4,3));
+// console.log(calc(5,6));
+// console.log(calc(10,6));
 
 function ret(){
     let num = 50;
@@ -36,4 +36,18 @@ logger();
 
 
 
-const  calc =(a,b) => a = b;
+const  calc =(a,b) => {return a = b} ;
+
+
+
+const usdCurr=28;
+const euroCurr=32;
+
+function convert (amount,curr) {
+
+console.log(curr * amount);
+
+}
+
+convert(500,usdCurr);
+convert(500,euroCurr);
